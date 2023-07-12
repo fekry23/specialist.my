51e330121847c18238b86b060ddf9522056f58c1
Fekry Saharudin <fekrysaharudins@gmail.com>
2023-07-12 20:59:12 +0800

    production

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,9 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!********************************!*\
-  !*** ./resources/js/footer.js ***!
-  \********************************/
-var date = new Date().getFullYear();
-document.getElementById("footer-date").innerHTML = date;
-/******/ })()
-;
+(()=>{var e=(new Date).getFullYear();document.getElementById("footer-date").innerHTML=e})();
